f6f4c6fec898ad5db2b2a7b418db7e18a435d082
Ghozy Arif Fajri <gojigeje@gmail.com>
2014-03-19 14:12:53 +0700

    add: quiet mode

--- speak-tts/speak.gmail.sh
+++ speak-tts/speak.gmail.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
+# ----------------------------------------------------------------------------------
+# @name    : speak.gmail.sh
+# @version : 0.1
+# @date    : 2014/03/19 04:31 WIB
+#
+# ABOUT
+# ----------------------------------------------------------------------------------
+# Simple bash script to get new email count from gmail.
+#
+# AUTHOR
+# ----------------------------------------------------------------------------------
+# Ghozy Arif Fajri <http://github.com/gojigeje>
+#
 
 cd /root/script/
+
+if [ -z "$1" ]
+  then
+    echo "non-quiet mode" > /dev/null
+  else
+    if [ "$1" = "-q" ]; then
+      QUIET="-q"
+    fi
+fi
 
 TMPDIR="/root/script"
 # TMPDIR="$HOME/Desktop"
@@ -11,8 +33,8 @@ if [ -s "$TMPDIR/inbox" ]; then
   if [ $new = "0" ]; then
     echo "You have no new message"
   else
-    bash speak.sh "Hi Goji!. You have $new new messages in your gmail inbox"
+    bash speak.sh "Hi Goji!. You have, $new, new email. in your gmail inbox" $QUIET
   fi
 else 
-  echo  "kosong"
+  echo "kosong"
 fi
