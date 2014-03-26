03447a4ec6a9357a788724161283cc62c7699688
Ghozy Arif Fajri <gojigeje@gmail.com>
2014-03-26 19:08:03 +0700

    fix: cek | add: reload

--- control/control.mjpgstreamer.sh
+++ control/control.mjpgstreamer.sh
@@ -13,12 +13,6 @@
 # Ghozy Arif Fajri <http://github.com/gojigeje>
 #
 
-# startup
-#   cek last shutdown
-# cek tiap 30 menit
-#   pake wget?
-#   pake -o file
-
 WEBFOLDER="/website/arsip"
 TAHUNBULAN=$(date +'%Y-%m')
 TANGGAL=$(date +'%d')
@@ -33,15 +27,28 @@ startup() {
   mjpg_streamer -i "input_uvc.so --fps "5" -d /dev/video0 -y" -o "output_http.so -w /www/webcam/"  -o "output_file.so -f $ARSIPFOLDER -d 900000" -b > /dev/null 2>&1
 }
 
+# access main server api
+message_started() {
+  curl -s "http://192.168.2.10/api/?code=gojimini2.cctv_successfully_started" > /dev/null 2>&1
+}
+message_not_responding() {
+  curl -s "http://192.168.2.10/api/?code=gojimini2.cctv_not_responding" > /dev/null 2>&1
+}
+message_system_ready() {
+  curl -s "http://192.168.2.10/api/?code=gojimini2.system_is_ready" > /dev/null 2>&1
+}
+
 cek_wget() {
   cd /root/script
   getDate
   wget -q "http://localhost/?action=stream" -O snapshot.jpg &
   PID=$!
   sleep 10
-  if [ `ps | grep $PID]` -ne '' ; then
+  PSPID=$(ps | grep $PID | grep -v grep)
+  if [ "$PSPID" != "" ]; then
     # macet
-    kill $PID
+    # kill $PID
+    echo "kill $PID !!!"
     CEKWGET="KO"
     rm snapshot.jpg
   else
@@ -54,7 +61,7 @@ cek_file() {
   cd "$ARSIPFOLDER"
   echo ":)" > .tes
 
-  if [ -f ".tes"]; then
+  if [ -f ".tes" ]; then
     CEKFILE="OK"
     rm .tes
   else
@@ -73,27 +80,108 @@ cek_running() {
 }
 
 case "$1" in
-  "1" ) # 1s
-    
+  "startup"|"start" )
+    echo ""
+    echo "$(date +%Y-%m-%d_%H:%M:%S) - Starting mjpg_streamer up.."
+
+    cek_running
+    if [ "$smjpg" = "ON" ]; then
+      echo "$(date +%Y-%m-%d_%H:%M:%S) - mjpg_streamer is already started!"
+      exit 1
+    fi
+
+    startup
+    sleep 2
+    cek_running
+    if [ "$smjpg" = "OFF" ]; then
+      echo "$(date +%Y-%m-%d_%H:%M:%S) - mjpg_streamer couldn't start? Starting again.. (1)"
+      startup
+      cek_running
+      if [ "$smjpg" = "OFF" ]; then
+        echo "$(date +%Y-%m-%d_%H:%M:%S) - mjpg_streamer couldn't start? Starting again.. (2)"
+        startup
+        cek_running
+        if [ "$smjpg" = "OFF" ]; then
+          echo "$(date +%Y-%m-%d_%H:%M:%S) - mjpg_streamer couldn't start? Starting again.. (3)"
+          echo "$(date +%Y-%m-%d_%H:%M:%S) - Giving up :( -- reboot?"
+          # force reboot
+          message_not_responding
+          reboot -f
+        fi
+      fi
+    else
+      # successfully started
+      message_started
+    fi
   ;;
 
-  "2" ) # 3s
-    
+  "cek" )
+    echo ""
+    echo "$(date +%Y-%m-%d_%H:%M:%S) - Checking mjpg_streamer health.."
+    cek_running
+    if [ "$smjpg" = "ON" ]; then
+      echo "$(date +%Y-%m-%d_%H:%M:%S) - Checking disk write access.."
+      cek_file
+      if [ "$CEKFILE" = "OK" ]; then
+        echo "$(date +%Y-%m-%d_%H:%M:%S) - Disk write access OK"
+        echo "$(date +%Y-%m-%d_%H:%M:%S) - Checking snapshot using wget.."
+        cek_wget
+        if [ "$CEKWGET" = "OK" ]; then
+          echo "$(date +%Y-%m-%d_%H:%M:%S) - Snapshot OK"
+          echo "$(date +%Y-%m-%d_%H:%M:%S) - System is normal :)"
+        else
+          echo "$(date +%Y-%m-%d_%H:%M:%S) - Snapshot FAIL!"
+          echo "$(date +%Y-%m-%d_%H:%M:%S) - reboot? (wget_fail)"
+          # force reboot
+          message_not_responding
+          reboot -f
+        fi
+      else
+        echo "$(date +%Y-%m-%d_%H:%M:%S) - Disk write FAIL!"
+        echo "$(date +%Y-%m-%d_%H:%M:%S) - reboot? (write_fail)"
+        # force reboot
+        message_not_responding
+        reboot -f
+      fi
+    else
+      echo "$(date +%Y-%m-%d_%H:%M:%S) - mjpg_streamer not running!"
+    fi
   ;;
 
-  "3" ) # 5s
-    
-  ;;
+  "reload" )
+    cek_running
+    if [ "$smjpg" = "OFF" ]; then
+      echo "$(date +%Y-%m-%d_%H:%M:%S) - mjpg_streamer is not running!"
+      exit 1
+    fi
 
-  "4" ) # 7s
-    
+    echo "$(date +%Y-%m-%d_%H:%M:%S) - killing mjpg_streamer.."
+    killall mjpg_streamer
+    sleep 2
+    cek_running
+    if [ "$smjpg" = "ON" ]; then
+      echo "$(date +%Y-%m-%d_%H:%M:%S) - can't kill mjpg_streamer! killing again (1)"
+      killall mjpg_streamer
+      cek_running
+      if [ "$smjpg" = "ON" ]; then
+        echo "$(date +%Y-%m-%d_%H:%M:%S) - can't kill mjpg_streamer! killing again (2)"
+        killall mjpg_streamer
+        cek_running
+        if [ "$smjpg" = "ON" ]; then
+          echo "$(date +%Y-%m-%d_%H:%M:%S) - can't kill mjpg_streamer! killing again (3)"
+          echo "$(date +%Y-%m-%d_%H:%M:%S) - Giving up :( -- reboot?"
+          # force reboot
+          message_not_responding
+          reboot -f
+        fi
+      fi
+    else
+      echo "$(date +%Y-%m-%d_%H:%M:%S) - killed!"
+      $0 start
+    fi
   ;;
-
-  "5" ) # 10s
-    
-  ;; 
 
   *)
-    echo "?"
+    echo "Wrong parameter!"
   ;;
 esac
